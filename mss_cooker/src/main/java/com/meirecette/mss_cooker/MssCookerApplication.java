aeb0019b163c3899326d1bc3e99b634f2d63b5be
Mei423 <chunmei-li@hotmail.com>
2021-09-10 00:13:08 +0200

    scrapping code with python

---
+++ mss_cooker/src/main/java/com/meirecette/mss_cooker/MssCookerApplication.java
@@ -0,0 +1,13 @@
+package com.meirecette.mss_cooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssCookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssCookerApplication.class, args);
+	}
+
+}
